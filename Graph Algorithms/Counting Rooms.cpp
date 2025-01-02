cfd131c2d897fadf5135d38e18460c14cef4fc0f
Shivam Singh Parihar <iit2022130@iiita.ac.in>
2025-01-02 19:51:47 +0530

    created a new folder and .cpp file

---
+++ Graph Algorithms/Counting Rooms.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define endl "\n"
+#define int long long
+
+void func(){
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t=1;
+    cin>>t;
+    while(t--){
+        func();
+    }
+    return 0;
+}
