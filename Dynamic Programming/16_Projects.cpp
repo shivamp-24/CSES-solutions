0468dd3820a9b0ae9d1b0fdca13adbe940162443
Shivam Singh Parihar <iit2022130@iiita.ac.in>
2025-02-16 23:27:07 +0530

    Projects Done Using Map DP

--- Dynamic Programming/16_Projects.cpp
+++ Dynamic Programming/16_Projects.cpp
@@ -7,9 +7,49 @@ using namespace std;
 
 void func() {
     int n; cin >> n;
+    map<int, vector<pair<int,int>>> mp;
+    set<int> endDays;
     for(int i=0; i<n; i++){
-        
+        int a, b, p; cin >> a >> b >> p;
+        mp[b].push_back({a, p});
     }
+    map<int, int> dp;
+    //dp[x] -> maximum reward when last ending day was x
+    //base case
+    auto it = mp.begin();
+    for(auto [start, price]: it->second){
+        dp[it->first] = max(dp[it->first], price);
+    }
+    endDays.insert(it->first);
+    it++;
+    //fill dp
+    while(it != mp.end()){
+        int currEnd = it->first;
+        //pick
+        int pick = 0;
+        for(auto [start, price]: it->second){
+            auto it1 = endDays.lower_bound(start);
+            if(it1 == endDays.begin()){ //no end exists before start
+                pick = max(pick, price);
+            }
+            else{ //end exists before start
+                it1--;
+                pick = max(pick, price + dp[(*it1)]);
+            }
+        }
+        //skip
+        int skip = dp[*(--endDays.end())];
+        dp[currEnd] = max(pick, skip);
+
+        endDays.insert(currEnd);
+        it++;
+    }
+    //answer will be max in dp
+    int ans = 0;
+    for(auto x: dp){
+        ans = max(ans, x.second);
+    }
+    cout << ans << endl;
 }
 
 signed main(){
