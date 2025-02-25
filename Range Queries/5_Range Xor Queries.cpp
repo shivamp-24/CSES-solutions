3e25af1603cd23e8119557ce0349f72596543ee2
Shivam Singh Parihar <iit2022130@iiita.ac.in>
2025-02-25 18:48:20 +0530

    Prefix xor use

---
+++ Range Queries/5_Range Xor Queries.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define mod 1000000007
+#define endl "\n"
+#define int long long
+
+void func() {
+    int n, q; cin >> n >> q;
+    vector<int> arr(n), pre(n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+        pre[i] = arr[i];
+        if(i > 0) pre[i] ^= pre[i-1];
+    }
+    while(q--) {
+        int a, b; cin >> a >> b;
+        a--, b--;
+        if(a == 0) cout << pre[b] << endl;
+        else cout << (pre[b] ^ pre[a-1]) << endl;
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        func();
+    }
+    return 0;
+}
