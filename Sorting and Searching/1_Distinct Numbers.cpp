bb77bf42de143adef21db3ba6094683a1e30cb7a
Shivam Singh Parihar <iit2022130@iiita.ac.in>
2025-03-03 16:06:44 +0530

    Sort to find uniques

---
+++ Sorting and Searching/1_Distinct Numbers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define mod 1000000007
+#define endl "\n"
+#define int long long
+
+void func() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    int ans = 1;
+    for(int i=1; i<n; i++) {
+        if(arr[i] != arr[i-1]) ans++;
+    }
+    cout << ans << endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        func();
+    }
+    return 0;
+}
